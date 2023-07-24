cfe4fe9995a5239fd79bbf0d31e8b22e80717bb1
Américo <americochiclana@gmail.com>
2023-07-24 12:50:39 +0200

    user routes

--- api-rest/controllers/user.js
+++ api-rest/controllers/user.js
@@ -3,6 +3,14 @@ const test = (req,res) =>{
     return res.status(200).send({message: "Message send from controller: user"})
 }
 
+const register = (req,res) =>{
+    return res.status(200).send({
+        message: "Register working"
+    })
+}
+
+
 module.exports = {
-    test
+    test,
+    register
 }
