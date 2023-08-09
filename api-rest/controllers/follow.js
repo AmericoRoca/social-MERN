2391a07feccba737d31f02f134d32ecde26a94cd
Américo <americochiclana@gmail.com>
2023-08-09 11:01:06 +0200

    unfollow

--- api-rest/controllers/follow.js
+++ api-rest/controllers/follow.js
@@ -39,55 +39,41 @@ const save = async (req, res) => {
     }
   };
 
-  const unfollow = async(req,res) => {
-
-
+  const unfollow = async (req, res) => {
     try {
+        // Obtener el ID del usuario
+        const id = req.user.id;
 
-      //Get user id
-      let id = req.user.id;
+        // Obtener el ID del usuario seguido
+        const followedId = req.params.id;
 
+        // Ejecutar la eliminación
+        const followDeleted = await Follow.findOneAndDelete({
+            user: id,
+            followed: followedId
+        });
 
-      // Find coincidences
-      const followedId = req.params.id;
-
-
-      // Exec Remove
-      let followStored = Follow.find({
-        "user": userId,
-        "followed": followedId
-      }).remove((error, followDeleted) =>{
-
-        if(error || ! followDeleted){
-          return res.status(500).send({
-            message: "Success",
-            status: "Success",
-            identity: req.user,
-            follow: followStored,
-          });
+        if (!followDeleted) {
+            return res.status(500).send({
+                message: "Error deleting follow.",
+                status: "Error",
+            });
         }
 
         return res.status(200).send({
-          message: "Success",
-          status: "Success",
-          identity: req.user,
-          follow: followStored,
+            message: "Successfully unfollowed.",
+            status: "Success",
+            identity: req.user,
+            follow: followDeleted,
         });
-
-
-      })
-
-      
     } catch (error) {
-      
-      return res.status(500).send({
-        message: "An unexpected error occurred during the follow process.",
-        status: "Error",
-      });
+        return res.status(500).send({
+            message: "An unexpected error occurred during the unfollow process.",
+            status: "Error",
+        });
     }
+}
 
-    
-  }
   
 
 module.exports = {
