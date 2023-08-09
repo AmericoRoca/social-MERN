731d92287bdf27dfd7cf9916153c356ebb52b0e1
Américo <americochiclana@gmail.com>
2023-08-09 16:38:44 +0200

    Follows

--- api-rest/controllers/follow.js
+++ api-rest/controllers/follow.js
@@ -1,7 +1,11 @@
 //Import dependencies
 const Follow = require("../models/Follow")
 const User = require("../models/User")
+const mongoosePaginate = require("mongoose-pagination")
 
+
+//import service
+const followService = require("../services/followUserIds")
 
 
 
@@ -74,9 +78,72 @@ const save = async (req, res) => {
     }
 }
 
-  
+//(siguiendo)
+const following = async (req, res) => {
+  try {
+      // Obtener el ID del usuario identificado
+      let userId = req.user.id;
+
+      // Verificar si el ID se está pasando por la URL
+      if (req.params.id) userId = req.params.id;
+
+      // Verificar si se está recibiendo el número de página, si no es así, usar la página 1
+      let page = 1;
+      if (req.params.page) {
+          page = req.params.page;
+      }
+
+      // Usuarios por página
+      const itemsPerPage = 5;
+
+      // Encontrar el total de usuarios que sigue
+      const totalFollows = await Follow.countDocuments({ user: userId });
+
+      // Encontrar los usuarios que sigue y poblar los datos de los usuarios
+      const follows = await Follow.find({ user: userId })
+          .populate('followed', "-password -role -__v")
+          .skip((page - 1) * itemsPerPage)
+          .limit(itemsPerPage)
+          .exec();
+
+      let followUserId = await followService.followUserIds(req.user.id);
+
+      return res.status(200).send({
+          message: "Successfully following.",
+          status: "Success",
+          follows,
+          totalPages: Math.ceil(totalFollows / itemsPerPage),
+          user_following: followUserId.following,
+          user_follow_me: followUserId.followers
+      });
+  } catch (error) {
+      return res.status(500).send({
+          message: "An unexpected error occurred while fetching following users.",
+          status: "Error",
+      });
+  }
+}
+
+//(Soy seguido)
+const followers = async(req,res) => {
+  try {
+    
+  } catch (error) {
+    return res.status(500).send({
+      message: "An unexpected error occurred during the unfollow process.",
+      status: "Error",
+    });
+  }
+
+  return res.status(200).send({
+    message: "Successfully followers.",
+    status: "Success",
+  });
+}
 
 module.exports = {
     save,
-    unfollow
+    unfollow,
+    following,
+    followers
 }
