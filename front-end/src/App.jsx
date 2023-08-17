0870a9a50c75848e759f3e9142e893875ce7f38c
Américo <americochiclana@gmail.com>
2023-08-17 12:23:14 +0200

    creating the front-end

---
+++ front-end/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App(){
+  return (
+    <div className='layout'>
+      <h1>Proyecto React</h1>
+
+    </div>
+  )
+}
+
+export default App
---
+++ front-end/src/main.jsx
@@ -0,0 +1,19 @@
+//Importaciones obligatorias
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+
+//importar assets
+//import './assets/fonts/fontawesome-free-6.1.2-web'
+import './assets/css/normalize.css'
+import './assets/css/styles.css'
+import './assets/css/responsive.css'
+
+
+//Arrancar app React
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    <App />
+
+)
