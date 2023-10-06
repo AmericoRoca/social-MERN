c79de68045327349d114d00d0c7a71e826eb0780
Américo <americochiclana@gmail.com>
2023-10-06 14:52:03 +0200

    working untill here!

--- front-end/src/components/user/People.jsx
+++ front-end/src/components/user/People.jsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from "react";
+import avatar from "../../assets/img/user.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { Global } from "../../helpers/Global";
-import { UserList } from "./UserList";
+import useAuth from "../../hooks/useAuth";
 
 export const People = () => {
-
-
+  const { auth } = useAuth();
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(true)
-  const [more, setMore] = useState(true)
+  const [more, setMore] = useState(true);
   const [following, setFollowing] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getUsers(1);
   }, []);
 
   const getUsers = async (nextPage = 1) => {
-    //hacer peticion
-    const request = await fetch(Global.url + "user/list/"+nextPage, {
+    //Efecto de carga
+    setLoading(true);
+
+    //Peticion sacar usuarios
+    const request = await fetch(Global.url + "user/list/" + nextPage, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -27,31 +32,69 @@ export const People = () => {
 
     const data = await request.json();
 
-    
-
-    //crear estado para listar
+    //Crear estado para poder listarlos
     if (data.users && data.status == "Success") {
-
       let newUsers = data.users;
 
-      if(users.length >= 1){
+      if (users.length >= 1) {
         newUsers = [...users, ...data.users];
       }
-       
+
       setUsers(newUsers);
-      setFollowing(data.following)
+      setFollowing(data.following);
       setLoading(false);
 
-      if(users.length >= (data.total - data.users.length)){
+      //Paginacion
+      if (users.length >= data.total - data.users.length) {
         setMore(false);
       }
-
     }
-
-    //hacer paginacion
   };
 
+  const nextPage = () => {
+    let next = page + 1;
+    setPage(next);
+    getUsers(next);
+  };
 
+  const follow = async (userId) => {
+    //peticion para guardar el follow
+    const request = await fetch(Global.url + "follow/save", {
+      method: "POST",
+      body: JSON.stringify({ followed: userId }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"),
+      },
+    });
+
+    const data = await request.json();
+
+    //actualizar el follow
+    if (data.status == "Success") {
+      setFollowing([...following, userId]);
+    }
+  };
+  const unfollow = async (userId) => {
+    //peticion para guardar el follow
+    const request = await fetch(Global.url + "follow/unfollow/" + userId, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"),
+      },
+    });
+
+    const data = await request.json();
+
+    //actualizar el follow
+    if (data.status == "Success") {
+      let filterFollowings = following.filter(
+        (followingUserId) => userId !== followingUserId
+      );
+      setFollowing(filterFollowings);
+    }
+  };
 
   return (
     <>
@@ -59,15 +102,78 @@ export const People = () => {
         <h1 className="content__title">Gente</h1>
       </header>
 
-      <UserList users={users} 
-                getUsers={getUsers} 
-                following={following}
-                setFollowing={setFollowing}
-                loading={loading}
-                more={more}
-                page={page}
-                setPage={setPage}
-        />
+      <div className="content__posts">
+        {loading ? "Cargando..." : ""}
+
+        {users.map((user) => {
+          return (
+            <article className="posts__post" key={user._id}>
+              <div className="post__container">
+                <div className="post__image-user">
+                  <a href="#" className="post__image-link">
+                    {user.image != "default.png" && (
+                      <img
+                        src={Global.url + "user/getAvatar/" + user.image}
+                        className="post__user-image"
+                        alt="Foto de perfil"
+                      />
+                    )}
+                    {user.image == "default.png" && (
+                      <img
+                        src={avatar}
+                        className="post__user-image"
+                        alt="Foto de perfil"
+                      />
+                    )}
+                  </a>
+                </div>
+
+                <div className="post__body">
+                  <div className="post__user-info">
+                    <a href="#" className="user-info__name">
+                      {user.name}
+                      {user.surname}
+                    </a>
+                    <span className="user-info__divider"> | </span>
+                    <a href="#" className="user-info__create-date">
+                      {user.created_at}
+                    </a>
+                  </div>
+
+                  <h4 className="post__content">{user.bio}</h4>
+                </div>
+              </div>
+              {user._id != auth._id && (
+                <div className="post__buttons">
+                  {!following.includes(user._id) && (
+                    <button
+                      className="post__button post__button--green"
+                      onClick={() => follow(user._id)}
+                    >
+                      Seguir
+                    </button>
+                  )}
+                  {following.includes(user._id) && (
+                    <button
+                      className="post__button"
+                      onClick={() => unfollow(user._id)}
+                    >
+                      Dejar de seguir
+                    </button>
+                  )}
+                </div>
+              )}
+            </article>
+          );
+        })}
+        {more && (
+          <div className="content__container-btn">
+            <button className="content__btn-more-post" onClick={nextPage}>
+              Ver mas publicaciones
+            </button>
+          </div>
+        )}
+      </div>
     </>
   );
 };
