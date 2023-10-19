b829a57b94a049e34334f82900050c6e8b887cc6
Américo <americochiclana@gmail.com>
2023-10-19 16:05:56 +0200

    header, favicon and styles

--- front-end/src/components/layout/private/Header.jsx
+++ front-end/src/components/layout/private/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Nav } from './Nav'
+import avatar from '../../../assets/img/MERN-SOCIAL.png'
 
 export const Header = () => {
   return (
     <header className="layout__navbar">
 
     <div className="navbar__header">
-      <a href="#" className="navbar__title">ReactSocial</a>
+      <a href="#" className="navbar__title">
+        <img src={avatar} alt="" className='image-header'/>
+      </a>
     </div>
 
     <Nav/>
--- front-end/src/components/layout/public/Header.jsx
+++ front-end/src/components/layout/public/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Nav } from './Nav'
 import { Link } from 'react-router-dom'
+import avatar from '../../../assets/img/MERN-SOCIAL.png'
 
 export const Header = () => {
   return (
     <header className="layout__navbar">
 
     <div className="navbar__header">
-      <Link to="/login" className="navbar__title">ReactSocial</Link>
+      <Link to="/login" className="navbar__title"><img src={avatar} alt="" className='image-header'/></Link>
     </div>
 
     <Nav/>
